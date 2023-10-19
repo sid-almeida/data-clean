8def980aba71c97b986da6d9d2fcda4ee4dc7157
Sidnei Almeida <117240480+sid-almeida@users.noreply.github.com>
2023-10-19 12:11:06 -0300

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,16 @@ import re
 # configurei o streamlit para abrir em wide mode
 st.set_page_config(layout="wide")
 
+# funçao para formatar cpf
+def format_cpf_column(data):
+    # Remove dígitos especiais e espaços
+    data["CPF_PARTICIPANTE"] = data["CPF_PARTICIPANTE"].str.replace(r'[.-]', '', regex=True)
+    # Adiciona um "0" no início dos valores que não começam com "0"
+    data["CPF_PARTICIPANTE"] = data["CPF_PARTICIPANTE"].apply(lambda x: '0' + x if not x.startswith('0') else x)
+    # Formata os valores para o formato "000.000.000-00"
+    data["CPF_PARTICIPANTE"] = data["CPF_PARTICIPANTE"].str.replace(r'(\d{3})(\d{3})(\d{3})(\d{2})', r'\1.\2.\3-\4', regex=True)
+    return data
+
 with st.sidebar:
     st.image("Brainize Tech(1).png", width=250)
     st.write('---')
@@ -18,10 +28,12 @@ if choice == "Limpeza Automatizada":
     # criei um uploader para fazer upload do arquivo .csv
     uploaded_file = st.file_uploader("Escolha um arquivo .csv", type="csv")
     if uploaded_file is not None:
+        # criei um selectbox para selecionar o separador do arquivo
+        sep = st.selectbox("Selecione o separador do arquivo", [",", ";", "|"])
         # salvei o nome do arquivo em uma variável
         filename = uploaded_file.name
         # transformei o arquivo em um dataframe com separador de ponto e vírgula
-        data = pd.read_csv(uploaded_file, sep=";", encoding="latin1")
+        data = pd.read_csv(uploaded_file, sep=sep, encoding="latin1")
         st.subheader("Dataframe")
         st.write(data)
         # converti todas as colunas para string
@@ -34,7 +46,7 @@ if choice == "Limpeza Automatizada":
         # criei um botão para executar a limpeza
         if st.button("Executar Limpeza"):
             # excluí as colunas que não foram selecionadas
-            data = data[colunas]
+            data = data[colunas].astype(str)
             # excluí linhas vaizas
             data = data.dropna(how="all")
             # removí os espaços em branco no início e no fim de cada string
@@ -44,8 +56,8 @@ if choice == "Limpeza Automatizada":
             # caso no título da coluna tenha a palavra "NOME", todos os valores devem começar com letra maiúscula no início de cada palavra
             data = data.apply(lambda x: x.str.title() if "NOME" in x.name else x)
             data = data.apply(lambda x: x.str.zfill(11) if "CPF" in x.name and len(x) < 14 else x)
-            # na coluna CPF_PARTICIPANTE todos os valores devem ter 14 caracteres e estarem no formato 000.000.000-00
-            data["CPF_PARTICIPANTE"] = data["CPF_PARTICIPANTE"].apply(lambda x: f"{x[:3]}.{x[3:6]}.{x[6:9]}-{x[9:]}")
+            # Aplicando a função format_cpf_column
+            data = format_cpf_column(data)
 
 
             # Mostrando o resultado da limpeza
